616c11c0a53600a23854fc986a54026255c407fa
mariKaren <karenbmari@gmail.com>
2025-07-01 21:03:55 +0200

    Creación de la estructura base de Home

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,8 +1,63 @@
-export const Home = () => {
+
+import { NavLink} from "react-router";
+import BookGrid from "../components/BookGrid";
+import GenreLinks from "../components/GenreLinks";
+
+// Por ahora libros destacados hardcodeados
+const featuredBooks = [
+    {
+        id: "1",
+        title: "El Quijote",
+        author: "Miguel de Cervantes",
+        cover: "https://covers.openlibrary.org/b/id/8226191-L.jpg",
+    },
+    {
+        id: "2",
+        title: "Cien años de soledad",
+        author: "Gabriel García Márquez",
+        cover: "https://covers.openlibrary.org/b/id/8231851-L.jpg",
+    },
+    {
+        id: "3",
+        title: "La Sombra del Viento",
+        author: "Carlos Ruiz Zafón",
+        cover: "https://covers.openlibrary.org/b/id/8225636-L.jpg",
+    },
+    {
+        id: "4",
+        title: "La Sombra del Viento",
+        author: "Carlos Ruiz Zafón",
+        cover: "https://covers.openlibrary.org/b/id/8225636-L.jpg",
+    },
+];
+
+export default function Home(){
     return (
-        <div className="mx-auto">
-        <h1 className="text-3xl font-bold">Home</h1>
-        <p className="mt-4">Bienvenido a la página de inicio.</p>
+        <div className="space-y-12">
+        {/* Portada */}
+            <section className="relative text-beige bg-brown rounded-lg overflow-hidden h-64 flex items-center justify-center px-6">
+                <img src="src/assets/portada.jpg" alt="Portada Biblioteca" className="absolute inset-0 object-cover w-full h-full opacity-35"/>
+                <div className="relative z-10 text-center max-w-3xl">
+                    <h1 className="text-2xl md:text-4xl font-bold mb-5 drop-shadow-lg">
+                        Descubre tu próximo gran libro
+                    </h1>
+                    <NavLink to="/libros/busqueda" className="bg-beige text-brown font-semibold px-6 py-2 rounded-md hover:bg-yellow transition inline-block">
+                        Explorar ahora
+                    </NavLink>
+                </div>
+            </section>
+
+            {/* Destacados */}
+            <section>
+                <h2 className="text-2xl font-semibold mb-6">Libros Destacados</h2>
+                <BookGrid books={featuredBooks} />
+            </section>
+
+            {/* Géneros */}
+            <section>
+                <h2 className="text-2xl font-semibold mb-6">Explorar por Géneros</h2>
+                <GenreLinks/>
+        </section>
         </div>
-    )
+    );
 }
--- Frontend/src/routes/AppRouter.tsx
+++ Frontend/src/routes/AppRouter.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router";
 import Layout from "../layout/Layout";
 
-import {Home} from "../pages/Home";
+import Home from "../pages/Home";
 import {About} from "../pages/About";
 import {Contact} from "../pages/Contact";
 import {Wishlist} from "../pages/Wishlist";
