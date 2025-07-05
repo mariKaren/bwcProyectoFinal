5b7e9a79dfce77526c477b0d5007fd20ff3d50de
mariKaren <karenbmari@gmail.com>
2025-07-05 18:02:49 +0200

    Realización de lógica en BookDetail excepto escribir reseña

--- Frontend/src/pages/BookDetail.tsx
+++ Frontend/src/pages/BookDetail.tsx
@@ -12,6 +12,8 @@ export function BookDetail() {
     const [book, setBook] = useState<Book | null>(null);
     const [reviews, setReviews] = useState<Review[]>([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [isInWishlist, setIsInWishlist] = useState(false);
+    const [hasUserReviewed, setHasUserReviewed] = useState(false);
 
     useEffect(() => {
         api.get(`/books/${id}`)
@@ -34,22 +36,69 @@ export function BookDetail() {
             setIsLoading(false);
         });
 
-        // Si querés traer también reseñas:
-        api.get(`/reviews`)
+        //ESTABLECER QUE NO REALICE LA PETICION SI ISAUTENTICATED ES FALSE
+        // para tarer las reseñas
+        api.get(`/books/${id}/reviews`)
         .then((res) => {
-            const allReviews: Review[] = res.data.data;
-            const filtered = allReviews.filter((r) => r.book_id === Number(id));
-            console.log(filtered)
-            setReviews(filtered);
+            const reviewsData = res.data.data;
+            setReviews(reviewsData);
+            
+            if (isAuthenticated && user) {
+            const alreadyReviewed = reviewsData.some((r: Review) => r.user_id === user.id);
+            setHasUserReviewed(alreadyReviewed);
+            }
+        })
+        .catch((err) => {
+            console.error("Error al cargar reseñas", err);
+        });
+        console.log(id)
+        api.get(`/wishlist`)
+            .then((res) => {
+                console.log(res)
+                const found = res.data.data.some((item: any) => item.book_id == id);//ya que viene de qp
+                setIsInWishlist(found);
             })
-        .catch((err) => console.error("Error al cargar reseñas", err));
+            .catch((err) => {
+                console.error("Error al verificar wishlist:", err);
+            });
     }, [id]);
-
+    
     if (isLoading || loading) return <div>Cargando...</div>;//carga del auth o de los datos del libro
     if (!book) return <div>Libro no encontrado</div>;
 
     const isUser = isAuthenticated && user?.role === "user";
 
+    const handleDeleteReview = async (reviewId: number) => {//opcion de eliminar reseña
+        try {
+        await api.delete(`/reviews/${reviewId}`);
+        setReviews((prevReviews) => prevReviews.filter((r) => r.id !== reviewId));
+        setHasUserReviewed(false);
+        } catch (error) {
+        console.error("Error al eliminar la reseña:", error);}
+    };
+
+    const handleAddToWishlist = async () => {
+    try {
+        const res = await api.post('/wishlist', { book_id: book.id });
+        alert("Libro agregado a tu wishlist");
+        setIsInWishlist(true);
+    } catch (err: any) {
+        console.error(err);
+        alert("Error al agregar a wishlist");
+    }
+    };
+
+    const handleRemoveFromWishlist = ()=> {
+    api
+        .delete(`/wishlist/${id}`)
+        .then(() => {
+        console.log("Libro eliminado de wishlist ✅");
+        setIsInWishlist(false);
+        })
+        .catch((error) => {
+        console.error("Error al eliminar de wishlist:", error);
+        });
+    }
     return (
         <div className="p-6">
         <div className="flex flex-col md:flex-row gap-4">
@@ -73,33 +122,57 @@ export function BookDetail() {
 
         <div>
             <h3 className="text-xl font-semibold mb-2">Reseñas</h3>
+            { !isAuthenticated ? (
+                <p className="text-gray-500">Debe iniciar sesión para ver las reseñas</p>
+            ):(
+            <>
             {reviews.length > 0 ? (
             <ul className="space-y-2">
                 {reviews.map((r) => (
                 <li key={r.id} className="border p-2 rounded">
-                    {r.description}
+                <strong>{r.user?.name}</strong>: {r.description}
+                <div className="text-sm text-gray-500">Rating: {r.rating}/5</div>
+
+                {/* Si el usuario autenticado realizo la reseña o es admin puede eliminarla */}
+                {(user?.id === r.user_id || isAdmin) && (
+                <div className="mt-4 mb-2 space-x-2">
+                    <button
+                    onClick={() => handleDeleteReview(r.id)}
+                    className="bg-red-600 text-beige hover:bg-red-700 px-4 py-1"
+                    >
+                    Eliminar reseña
+                    </button>
+                </div>
+                )}
                 </li>
                 ))}
             </ul>
             ) : (
             <p className="text-gray-500">Aún no hay reseñas.</p>
             )}
+            </>
+            )}
         </div>
 
         <div className="mt-6">
             {isUser && (
             <>
-                <button
-                onClick={() => {/* lógica para wishlist */}}
-                className="bg-blue-600 text-white px-4 py-2 rounded mr-2"
-                >
-                Agregar a Wishlist
+                {isInWishlist ? (
+                <button onClick={handleRemoveFromWishlist} className="bg-red-600 text-white px-4 py-2 rounded">
+                    Eliminar de Wishlist
                 </button>
+                ) : (
+                <button onClick={handleAddToWishlist} className="bg-blue-600 text-white px-4 py-2 rounded">
+                    Agregar a Wishlist
+                </button>
+                )}
                 <button
                 onClick={() => {/* lógica para dejar reseña */}}
-                className="bg-green-600 text-white px-4 py-2 rounded"
-                >
-                Escribir Reseña
+                className={`px-4 py-2 rounded text-white ${
+                hasUserReviewed ? "bg-gray-400 cursor-not-allowed" : "bg-green-600 hover:bg-green-700"
+                }`}
+                disabled={hasUserReviewed}>
+                {hasUserReviewed ? "Libro ya reseñado" : "Escribir Reseña"}
                 </button>
             </>
             )}
--- Frontend/src/types/Review.ts
+++ Frontend/src/types/Review.ts
@@ -1,6 +1,7 @@
 export interface Review {
     id: number;
     book_id:number;
+    user_id:number;
     user: {
         id: number;
         name: string;
