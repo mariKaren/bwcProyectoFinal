f483c780adeb8738e7f196cee09a88b77b6790b2
mariKaren <karenbmari@gmail.com>
2025-07-10 17:12:06 +0200

    Modificación del Header

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -6,12 +6,9 @@ import { useAuth } from "../context/useAuth";
 export default function Header() {
     const { isAuthenticated, logout,isAdmin } = useAuth();
     const [menuOpen, setMenuOpen] = useState(false);
-    const [dropdownOpen, setDropdownOpen] = useState(false);
     const [accountDropdownOpen, setAccountDropdownOpen] = useState(false);
     const location = useLocation();
     const navigate = useNavigate();
-
-    const isLibrosActive = location.pathname.startsWith("/libros");
 
     // Función para manejar el logout y redirigir
     const handleLogout = () => {
@@ -27,7 +24,7 @@ export default function Header() {
 
                 {/* Logo */}
                 <Link to="/" aria-label="Ir a la página de inicio">
-                    <img src="/src/assets/Book.png" alt="Logo de la libreria" className="h-20 w-auto" />
+                    <img src="/src/assets/Book.png" alt="Logo de la libreria" className="h-15 lg:h-20 w-auto" />
                 </Link>
 
                 {/* Botón hamburguesa (solo mobile) */}
@@ -40,103 +37,24 @@ export default function Header() {
                     <ul className="flex flex-col md:flex-row gap-4 md:gap-8 lg:gap-12 md:items-center text-brown w-full md:w-auto">
 
                         {/* Libros */}
-                        <li className="w-full md:w-auto relative">
-                            <button
-                                onClick={() => setDropdownOpen(!dropdownOpen)}
-                                className={`text-left font-semibold pr-2 py-1 md:pl-2 transition-all duration-300 ease-in-out md:inline-flex md:w-auto flex items-center ${
-                                isLibrosActive ? "text-orange" : ""
-                                }hover:border-b-2 hover:border-r-2 border-transparent hover:border-[var(--c-yellow)] hover:shadow-md`}
-                                aria-label="Abrir menú de libros"
+                        <li className="w-full md:w-auto">
+                            <NavLink
+                                to="/libros/busqueda"
+                                className={({ isActive }) =>`pr-2 py-1 md:pl-2 transition-all duration-200 font-semibold 
+                                ${isActive ? "text-orange " : ""} hover:text-red`
+                                }
+                                onClick={() => setMenuOpen(false)}
                             >
                                 Libros
-                            {/* SOLO flecha en mobile */}
-                                <span className="md:hidden ml-2"> {/* ml-2 */}
-                                    <svg
-                                        className={`w-4 h-4 transition-all duration-700 ${
-                                        dropdownOpen ? "rotate-180" : ""
-                                        }`}
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                    >
-                                        <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth={2}
-                                        d="M19 9l-7 7-7-7"
-                                        />
-                                    </svg>
-                                </span>
-                            </button>
-
-                            {/* Dropdown desktop*/}
-                            {dropdownOpen && (
-                                <ul className="hidden md:block absolute left-0 mt-2 bg-white shadow-md rounded p-2 w-max z-20">
-                                    <li>
-                                        <NavLink
-                                        to="/libros/busqueda"
-                                        onClick={() => {
-                                            setDropdownOpen(false);
-                                            setMenuOpen(false);
-                                        }}
-                                        className="block px-3 py-2 hover:bg-gray-100"
-                                        >
-                                        Búsqueda avanzada
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink
-                                        to="/libros/genero"
-                                        onClick={() => {
-                                            setDropdownOpen(false);
-                                            setMenuOpen(false);
-                                        }}
-                                        className="block px-3 py-2 hover:bg-gray-100"
-                                        >
-                                        Búsqueda por género
-                                        </NavLink>
-                                    </li>
-                                </ul>
-                            )}
-
-                            {/* Dropdown mobile*/}
-                            {dropdownOpen && (
-                                <ul className="flex flex-col pl-4 mt-1 gap-2 border-l-2 border-orange-500 md:hidden w-full">
-                                <li>
-                                    <NavLink
-                                    to="/libros/busqueda"
-                                    onClick={() => {
-                                        setDropdownOpen(false);
-                                        setMenuOpen(false);
-                                    }}
-                                    className="block text-brown hover:text-orange w-full"
-                                    >
-                                    Búsqueda avanzada
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink
-                                    to="/libros/genero"
-                                    onClick={() => {
-                                        setDropdownOpen(false);
-                                        setMenuOpen(false);
-                                    }}
-                                    className="block text-brown hover:text-orange w-full"
-                                    >
-                                    Búsqueda por género
-                                    </NavLink>
-                                </li>
-                                </ul>
-                            )}
+                            </NavLink>
                         </li>
-
-                        {/* Quienes somos */}
+                        
+                        {/* Sobre nosotros */}
                         <li className="w-full md:w-auto">
                             <NavLink
                                 to="/sobre-nosotros"
-                                className={({ isActive }) =>`pr-2 py-1 md:pl-2 transition-all duration-300 ease-in-out 
-                                ${isActive ? "text-orange font-semibold " : ""}hover:border-b-2 hover:border-r-2 border-transparent hover:border-[var(--c-yellow)] hover:shadow-md`
+                                className={({ isActive }) =>`pr-2 py-1 md:pl-2 transition-all duration-200 
+                                ${isActive ? "text-orange font-semibold " : ""} hover:text-red`
                                 }
                                 onClick={() => setMenuOpen(false)}
                             >
@@ -148,8 +66,8 @@ export default function Header() {
                         <li className="w-full md:w-auto">
                             <NavLink
                                 to="/contacto"
-                                className={({ isActive }) =>`pr-2 py-1 md:pl-2 transition-all duration-300 ease-in-out 
-                                ${isActive ? "text-orange font-semibold " : ""}hover:border-b-2 hover:border-r-2 border-transparent hover:border-[var(--c-yellow)] hover:shadow-md`
+                                className={({ isActive }) =>`pr-2 py-1 md:pl-2 transition-all duration-200  
+                                ${isActive ? "text-orange font-semibold" : ""} hover:text-red`
                                 }
                                 onClick={() => setMenuOpen(false)}
                             >
