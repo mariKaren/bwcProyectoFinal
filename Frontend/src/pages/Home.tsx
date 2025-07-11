f511c778c42f063c5ac2403a45c076b8efd36df5
mariKaren <karenbmari@gmail.com>
2025-07-11 22:01:46 +0200

    Separación de FeaturedBooks de page Home

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,30 +1,9 @@
 
 import { NavLink} from "react-router";
-import { useState,useEffect } from "react";
-import BookGrid from "../components/BookGrid";
-import type { Book } from "../types/book";
-import api from "../services/api";
-
+import FeaturedBooks from "../components/FeaturedBooks";
 
 export default function Home(){
-    const [featuredBooks, setFeaturedBooks] = useState<Book[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        const fetchFeaturedBooks = async () => {
-        try {
-            setIsLoading(true);
-            const response = await api.get("/featured-books");
-            console.log(response.data.data)
-            setFeaturedBooks(response.data.data);
-        } catch (error) {
-            console.error("Error fetching featured books:", error);
-        } finally {
-            setIsLoading(false);
-        }
-        };
-        fetchFeaturedBooks();
-    }, []);
+    
     return (
         <div className="space-y-12">
         {/* Portada */}
@@ -42,16 +21,7 @@ export default function Home(){
 
             {/* Destacados */}
             <section className="mb-8">
-                <h2 className="text-2xl md:text-3xl text-center text-red font-bold mb-10 drop-shadow-sm">Libros Destacados</h2>
-                <BookGrid books={featuredBooks} isLoading={isLoading} />
-                <div className="text-right"> 
-                    <NavLink
-                    to="/libros/busqueda"
-                    className="mt-6 inline-block bg-orange text-white font-semibold px-8 py-2 rounded-md hover:bg-orange-dark transition"
-                    >
-                    Ver más
-                    </NavLink>
-                </div>
+                <FeaturedBooks />
             </section>
 
         </div>
